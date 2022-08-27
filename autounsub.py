cc9fd1fa5c4cd87a4aa8b59f4f699e387b7b96a8
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-08-27 10:36:11 +1000

    Opening unsub links

--- autounsub.py
+++ autounsub.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import ezgmail
+import webbrowser
 import bs4
 import re
 
@@ -9,6 +10,7 @@ ezgmail.init()
 unsub_links = []
 unsub_regex = re.compile('|'.join(['opt-out', 'unsubscribe']), re.IGNORECASE)
 
+# Retrieve all unsubscribe links
 for thread in ezgmail.recent(5):
     for message in thread.messages:
         soup = bs4.BeautifulSoup(message.body, 'html.parser')
@@ -17,5 +19,6 @@ for thread in ezgmail.recent(5):
             if unsub_regex.search(elem.text):
                 unsub_links.append(elem.get('href'))
 
+# Open the links and perform try to unsubscribe
 for link in unsub_links:
-    print(link)
+    webbrowser.open(link)
